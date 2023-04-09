96078378055e380b7839eb8666d3d0c64b60348a
Alice Sales <avss2@cin.ufpe.br>
2023-04-09 15:29:39 -0300

    Atualiza fork resolvendo conflito

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -41,6 +41,9 @@ import { CadastraAlbumComponent } from './cadastra-album/cadastra-album.componen
 import { VisualizacaoAdminArtistasComponent } from './visualizacao-admin-artistas/visualizacao-admin-artistas.component';
 import { CadastraAlbumService } from './cadastra-album/cadastra-album.service';
 import { PlaylistAdminComponent } from './playlist-admin/playlist-admin.component';
+import { PlaylistsCategoriaComponent } from './playlists-categoria/playlists-categoria.component';
+import { PlaylistsCategoriaAdminComponent } from './playlists-categoria-admin/playlists-categoria-admin.component';
+import { BibliotecaUsuarioComponent } from './biblioteca-usuario/biblioteca-usuario.component';
 import { EditaMusicaComponent } from './edita-musica/edita-musica.component';
 import { EditaAlbumComponent } from './edita-album/edita-album.component';
 
@@ -76,6 +79,9 @@ import { EditaAlbumComponent } from './edita-album/edita-album.component';
     PlaylistAdminComponent,
     EditaMusicaComponent,
     EditaAlbumComponent
+    PlaylistsCategoriaComponent,
+    PlaylistsCategoriaAdminComponent,
+    BibliotecaUsuarioComponent
   ],
   imports: [
     BrowserModule,
